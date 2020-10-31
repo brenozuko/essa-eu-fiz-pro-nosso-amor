57b1c52961ee42581117dedffa73a0341f5ef7fb
Breno Costa Zukowski Marques <breno.zukowski@m2center.com.br>
2020-10-31 12:28:57 -0300

    First commit.

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="flex">
+
+<iframe src="https://open.spotify.com/embed/playlist/2CSgIPNxQwchNQMBUsEJhS" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style>
+</style>
