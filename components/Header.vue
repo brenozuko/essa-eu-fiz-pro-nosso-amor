93a333b97f0cbb8f43514d69312b5a24706d6096
Breno Costa Zukowski Marques <breno.zukowski@m2center.com.br>
2020-11-02 13:00:50 -0300

    Animations only on desktop.

--- components/Header.vue
+++ components/Header.vue
@@ -12,34 +12,34 @@ export default {};
 <style lang="scss">
 .neon {
   color: #fff;
-  animation: glow 1s ease-in-out infinite alternate;
   font-size: 40px;
   text-align: center;
   border: 1px solid #fff;
   box-shadow: 0 0 10px #fff;
   max-width: 90%;
-
+  text-shadow: 0 0 20px #fff, 0 0 30px #be2444, 0 0 40px #be2444,
+    0 0 50px #be2444, 0 0 60px #be2444, 0 0 70px #be2444, 0 0 80px #be2444;
+  filter: blur(0.8px);
 
   @apply rounded-lg mx-auto py-4 font-bold leading-tight mt-10 px-4;
+}
+
+@media (min-width: 768px) {
+  .neon {
+    font-size: 54px;
+    animation: glow 1s ease-in-out infinite alternate;
+  }
 
   @keyframes glow {
     from {
-      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff,
-        0 0 40px #fff, 0 0 50px #fff, 0 0 60px #fff,
-        0 0 70px #fff;
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #fff,
+        0 0 50px #fff, 0 0 60px #fff, 0 0 70px #fff;
     }
     to {
       text-shadow: 0 0 20px #fff, 0 0 30px #be2444, 0 0 40px #be2444,
         0 0 50px #be2444, 0 0 60px #be2444, 0 0 70px #be2444, 0 0 80px #be2444;
       filter: blur(0.8px);
     }
-  }
-}
-
-
-@media(min-width: 768px) {
-  .neon {
-    font-size: 54px;
   }
 }
 </style>
